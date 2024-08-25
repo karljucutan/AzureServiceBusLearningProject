f32c6b7bf6d4f95a7ddfc8f9a48991b4f16e3f18
karljucutan <karljucutan@gmail.com>
2024-08-25 19:26:39 +0800

    Service Bus Topic - sending messages.

---
+++ ServiceBusTopic/AzureServiceBus.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ServiceBusTopic
+{
+    public class AzureServiceBus
+    {
+        private string _topicName;
+        private string _connectionString;
+        private ServiceBusClient _serviceBusClient;
+
+        public AzureServiceBus()
+        {
+            _topicName = "apptopic";
+            _connectionString = "Endpoint=sb://kltjservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=oF9p9x0HGcCQTI+YbVK5zMEH/z8Up/+eY+ASbPEV/8Q=";
+            _serviceBusClient = new ServiceBusClient(_connectionString);
+        }
+
+        public async Task SendMessageAsyc(List<Order> orders)
+        {
+            var serviceBusSender = _serviceBusClient.CreateSender(_topicName);
+
+            using ServiceBusMessageBatch serviceBusMessageBatch = await serviceBusSender.CreateMessageBatchAsync();
+            int id = 0;
+            foreach (var order in orders)
+            {
+                ServiceBusMessage serviceBusMessage =
+                    new ServiceBusMessage(JsonSerializer.Serialize(order));
+                serviceBusMessage.ContentType = "application/json";
+                serviceBusMessage.ApplicationProperties.Add("Month", "August");
+
+                // set message id for duplicate detection, it wont send a message to a queue for a specific time with the same id.
+                //serviceBusMessage.MessageId = id.ToString(); id++;
+
+                serviceBusMessageBatch.TryAddMessage(serviceBusMessage);
+            }
+
+            await serviceBusSender.SendMessagesAsync(serviceBusMessageBatch);
+
+            Console.WriteLine($"Message sent");
+        }
+
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string? CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ ServiceBusTopic/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ServiceBusTopic;
+
+Console.WriteLine("Hello, World!");
+
+
+
+List<Order> orders = new List<Order>
+            {
+                new Order { Id = 1, CourseName = "AZ-204 Developer", Price = 10.99m },
+                new Order { Id = 2, CourseName = "AZ-104 Azure Admin", Price = 11.99m },
+                new Order { Id = 3, CourseName = "DP-203 Azure Data Engineer", Price = 12.99m }
+            };
+
+
+AzureServiceBus azureServiceBus = new AzureServiceBus();
+
+await azureServiceBus.SendMessageAsyc(orders);
